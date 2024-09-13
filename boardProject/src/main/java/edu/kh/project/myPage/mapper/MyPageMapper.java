71c01412ed841cf680909a595d3c255d77dca4f6
DESKTOP-RIENOSP\user1 <Sum-yujin>
2024-09-13 15:15:52 +0900

    240913_회원제 게시판 프로젝트

---
+++ boardProject/src/main/java/edu/kh/project/myPage/mapper/MyPageMapper.java
@@ -0,0 +1,23 @@
+package edu.kh.project.myPage.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import edu.kh.project.main.dto.Member;
+
+
+@Mapper // 상속받은 클래스 구현 + Bean 등록
+public interface MyPageMapper {
+
+	/** 회원 정보 수정
+	 * @param inputMember
+	 * @return result
+	 */
+	int updateInfo(Member inputMember);
+
+	/** 닉네임 중복 검사
+	 * @param input
+	 * @return result
+	 */
+	int checkNickname(String input);
+
+}
